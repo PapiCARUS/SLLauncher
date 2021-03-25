64c43e8e8dfa2b04f69fa985dd2b8e8bc7a2f1f0
PapiCARUS <81189319+PapiCARUS@users.noreply.github.com>
2021-03-25 03:33:16 +0100

    modif json

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,7 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+        const distroURL = 'https://api.jsonbin.io/b/605bf5ab1593c32d80bef4d1'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
